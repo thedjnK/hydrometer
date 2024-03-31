50d52bc8db68a21937086d58aae9049476d4b2b2
Jamie McCrae <spam@helper3000.net>
2024-03-31 10:46:08 +0100

    Add base application files

---
+++ app/src/main.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Jamie M.
+ *
+ * All right reserved. This code is not apache or FOSS/copyleft licensed.
+ */
+
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+	return;
+}
