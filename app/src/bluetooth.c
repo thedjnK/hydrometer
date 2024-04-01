52a5920a4bb23202246e0684c1a523a8a31efa03
Jamie McCrae <spam@helper3000.net>
2024-04-01 20:29:33 +0100

    Bluetooth updates

--- app/src/bluetooth.c
+++ app/src/bluetooth.c
@@ -19,8 +19,11 @@
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/bluetooth/services/bas.h>
+#include <zephyr/logging/log.h>
 
 #include "bluetooth.h"
+
+LOG_MODULE_DECLARE(app);
 
 #define HYDROMETER_SVC_UUID BT_UUID_128_ENCODE(0x6565432a, 0xafc0, 0x4812, 0xbb2a, 0x22532a48b000)
 
@@ -30,9 +33,19 @@ static const struct bt_uuid_128 hydrometer_control_char_uuid = BT_UUID_INIT_128(
         0x65, 0x65, 0x43, 0x2a, 0xaf, 0xc0, 0x48, 0x12,
 	0xbb, 0x2a, 0x22, 0x53, 0x2a, 0x48, 0xb0, 0x01);
 
-static const struct bt_uuid_128 hydrometer_reading_char_uuid = BT_UUID_INIT_128(
+static const struct bt_uuid_128 temperature_reading_char_uuid = BT_UUID_INIT_128(
         0x65, 0x65, 0x43, 0x2a, 0xaf, 0xc0, 0x48, 0x12,
 	0xbb, 0x2a, 0x22, 0x53, 0x2a, 0x48, 0xb0, 0x02);
+
+static const struct bt_uuid_128 roll_reading_char_uuid = BT_UUID_INIT_128(
+        0x65, 0x65, 0x43, 0x2a, 0xaf, 0xc0, 0x48, 0x12,
+	0xbb, 0x2a, 0x22, 0x53, 0x2a, 0x48, 0xb0, 0x03);
+
+static const struct bt_uuid_128 pitch_reading_char_uuid = BT_UUID_INIT_128(
+        0x65, 0x65, 0x43, 0x2a, 0xaf, 0xc0, 0x48, 0x12,
+	0xbb, 0x2a, 0x22, 0x53, 0x2a, 0x48, 0xb0, 0x04);
+
+static void bluetooth_advertise(void);
 
 static ssize_t write_hyrdrometer_control(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 					 const void *buf, uint16_t len, uint16_t offset,
@@ -41,27 +54,25 @@ static ssize_t write_hyrdrometer_control(struct bt_conn *conn, const struct bt_g
 	return 0;
 }
 
-static ssize_t read_u16(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+static ssize_t read_sensor(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 			void *buf, uint16_t len, uint16_t offset)
 {
-	const uint16_t *u16 = attr->user_data;
-	uint16_t value = sys_cpu_to_le16(*u16);
+	const double *value = (const double *)attr->user_data;
+//	uint16_t value = sys_cpu_to_le16(*u16);
 
-	return bt_gatt_attr_read(conn, attr, buf, len, offset, &value,
-				 sizeof(value));
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, value,
+				 sizeof(double));
 }
 
-/* Environmental Sensing Service Declaration */
+struct hydrometer_sensor_data {
+	double temperature;
+	double roll;
+	double pitch;
+};
 
-//struct hydrometer_sensor {
-//	double temperature;
-//	double roll;
-//	double pitch;
-//};
+static struct hydrometer_sensor_data sensor_data;
 
-//static bool simulate_temp;
-
-static void temp_ccc_cfg_changed(const struct bt_gatt_attr *attr,
+static void sensor_cccd_changed(const struct bt_gatt_attr *attr,
 				 uint16_t value)
 {
 //	simulate_temp = value == BT_GATT_CCC_NOTIFY;
@@ -71,18 +82,17 @@ static ssize_t read_es_measurement(struct bt_conn *conn,
 				   const struct bt_gatt_attr *attr, void *buf,
 				   uint16_t len, uint16_t offset)
 {
-/*	const struct es_measurement *value = attr->user_data;
-	struct read_es_measurement_rp rsp;
-
+/*
 	rsp.flags = sys_cpu_to_le16(value->flags);
 	rsp.sampling_function = value->sampling_func;
 	sys_put_le24(value->meas_period, rsp.measurement_period);
 	sys_put_le24(value->update_interval, rsp.update_interval);
 	rsp.application = value->application;
 	rsp.measurement_uncertainty = value->meas_uncertainty;
+*/
 
-	return bt_gatt_attr_read(conn, attr, buf, len, offset, &rsp,
-				 sizeof(rsp));*/
+//	return bt_gatt_attr_read(conn, attr, buf, len, offset, &rsp,
+//				 sizeof(rsp));
 return 0;
 }
 
@@ -110,16 +120,23 @@ BT_GATT_SERVICE_DEFINE(hydrometer_svc,
 	BT_GATT_CHARACTERISTIC(&hydrometer_control_char_uuid.uuid, BT_GATT_CHRC_WRITE,
 			       BT_GATT_PERM_WRITE, NULL, write_hyrdrometer_control, NULL),
 
-	/* Get reading */
-	BT_GATT_CHARACTERISTIC(&hydrometer_reading_char_uuid.uuid,
-			       BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,
-			       BT_GATT_PERM_READ,
-			       read_u16, NULL, 
-NULL),
-//&sensor_1.temp_value),
+	/* Temperature reading */
+	BT_GATT_CHARACTERISTIC(&temperature_reading_char_uuid.uuid,
+			       (BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY), BT_GATT_PERM_READ,
+			       read_sensor, NULL, &sensor_data.temperature),
+	BT_GATT_CCC(sensor_cccd_changed, (BT_GATT_PERM_READ | BT_GATT_PERM_WRITE)),
 
-	BT_GATT_CCC(temp_ccc_cfg_changed,
-		    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+	/* Roll reading */
+	BT_GATT_CHARACTERISTIC(&roll_reading_char_uuid.uuid,
+			       (BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY), BT_GATT_PERM_READ,
+			       read_sensor, NULL, &sensor_data.roll),
+	BT_GATT_CCC(sensor_cccd_changed, (BT_GATT_PERM_READ | BT_GATT_PERM_WRITE)),
+
+	/* Pitch reading */
+	BT_GATT_CHARACTERISTIC(&pitch_reading_char_uuid.uuid,
+			       (BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY), BT_GATT_PERM_READ,
+			       read_sensor, NULL, &sensor_data.pitch),
+	BT_GATT_CCC(sensor_cccd_changed, (BT_GATT_PERM_READ | BT_GATT_PERM_WRITE)),
 
 );
 
@@ -129,38 +146,39 @@ static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_UUID128_ALL, HYDROMETER_SVC_UUID),
 };
 
-static void connected(struct bt_conn *conn, uint8_t err)
+static void bluetooth_connected(struct bt_conn *conn, uint8_t err)
 {
 	if (err) {
-		printk("Connection failed (err 0x%02x)\n", err);
+		LOG_INF("Connection failed: %d", err);
+		bluetooth_advertise();
 	} else {
-		printk("Connected\n");
+		LOG_INF("Connected");
 	}
 }
 
-static void disconnected(struct bt_conn *conn, uint8_t reason)
+static void bluetooth_disconnected(struct bt_conn *conn, uint8_t reason)
 {
-	printk("Disconnected (reason 0x%02x)\n", reason);
+	printk("Disconnected: %d", reason);
+	bluetooth_advertise();
 }
 
 BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected = connected,
-	.disconnected = disconnected,
+	.connected = bluetooth_connected,
+	.disconnected = bluetooth_disconnected,
 };
 
-static void bt_ready(void)
+static void bluetooth_advertise(void)
 {
-	int err;
+	int rc;
 
-	printk("Bluetooth initialized\n");
+	rc = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
 
-	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
+	if (rc) {
+		LOG_ERR("Advertising failed to start: %d", rc);
 		return;
 	}
 
-	printk("Advertising successfully started\n");
+	LOG_INF("Advertising started");
 }
 
 #if 0
@@ -180,16 +198,21 @@ static void bas_notify(void)
 
 int setup_bluetooth(void)
 {
-	int err;
+	int rc;
 
-	err = bt_enable(NULL);
+	rc = bt_enable(NULL);
 
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return err;
+	if (rc) {
+		LOG_ERR("Bluetooth init failed: %d", rc);
+		return rc;
 	}
 
-	bt_ready();
+	bluetooth_advertise();
+
+sensor_data.temperature = 123.456;
+sensor_data.roll = 0.1;
+sensor_data.pitch = 5.5;
+
 
 //	while (1) {
 //		k_sleep(K_SECONDS(1));
